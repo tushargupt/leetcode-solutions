ec3bfd54ec2818ea8c97dba0213471318a616a95
Tushar Gupta <68987765+tushargupt@users.noreply.github.com>
2025-07-24 12:43:08 +0530

    Time: 12 ms (11.23%) | Memory: 25 MB (8.77%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) return {};
-        
-        sort(intervals.begin(), intervals.end());
-        
-        vector<vector<int>> result;
-        vector<int> currentInterval = intervals[0];
-        
-        for (int i = 1; i < intervals.size(); i++) {
-            if (currentInterval[1] >= intervals[i][0]) {
-                currentInterval[1] = max(currentInterval[1], intervals[i][1]);
-            } else {
-                result.push_back(currentInterval);
-                currentInterval = intervals[i];
-            }
+    vector<vector<int>> merge(vector<vector<int>>& matrix) {
+    sort(matrix.begin(), matrix.end());
+     vector<vector<int>> res;
+     res.push_back(matrix[0]);
+     int i=1;
+    while(i<matrix.size()){
+        vector<int> r = matrix[i];
+        if(r[0]>res[res.size()-1][1] && r[1]>res[res.size()-1][1])
+        {
+            res.push_back(r);
         }
-        
-        result.push_back(currentInterval);
-        
-        return result;
+        else{
+           res[res.size()-1][0] = min(res[res.size()-1][0], r[0]);
+            res[res.size()-1][1] = max(res[res.size()-1][1], r[1]);
+        }
+        i++;
+    }
+    return res;
     }
 };
