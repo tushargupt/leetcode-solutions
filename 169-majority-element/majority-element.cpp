c0f4d0b474e3dbaadc8016ff3f3499d7bd331c4e
Tushar Gupta <68987765+tushargupt@users.noreply.github.com>
2025-07-25 23:35:18 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (61.77%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0, candidate = 0;
+        for(int n: nums){
+            if(count==0){
+                candidate = n;
+            }
+            count += (n==candidate)?1:-1;
+        }
+        return candidate;
+    }
+};
