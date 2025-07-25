7bdc60c3c1a433e9153a5c752d5f6343377105fe
Tushar Gupta <68987765+tushargupt@users.noreply.github.com>
2025-07-25 19:32:56 +0530

    Time: 4 ms (39.15%) | Memory: 9.6 MB (74.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       if(s.length()!=t.length())
-       return false;
-       int alpha[26]={0};
-       for(char c: s){
-        alpha[c-'a']++;
-       }
-       for(char c: t){
-        alpha[c-'a']--;
-        if(alpha[c-'a']<0)
-        return false;
-       }
-       return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
     }
 };
